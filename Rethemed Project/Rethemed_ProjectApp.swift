15067c73a168d4708500c25c92d8ac687efaff4c
JackHyde <jack.hyde@school.barren.kyschools.us>
2023-03-22 08:17:24 -0500

    Initial Commit

---
+++ Rethemed Project/Rethemed_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rethemed_ProjectApp.swift
+//  Rethemed Project
+//
+//  Created by Jack Hyde on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Rethemed_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
